764de3cb287c63d86b4ebb786b378d3add05d978
MarkoNS1990 <marczecevic@gmail.com>
2020-10-18 12:39:11 +0200

    new func

--- booking.js
+++ booking.js
@@ -1,8 +1,14 @@
+const makeRes=document.querySelector("#makeRes");
 
 
-fetch('http://localhost:3000/destinations')
-.then(res=>res.json())
-.then(data=>{
+
+getData()
+
+function getData(){
+    
+    fetch('http://localhost:3000/destinations')
+    .then(res=>res.json())
+    .then(data=>{
     if(data.length>0){
         $(document).ready( function () {
             $('table').DataTable();
@@ -16,8 +22,11 @@ fetch('http://localhost:3000/destinations')
         temp+='<td>'+item.duration+'</td>'
         temp+='<td>'+item.price+'</td>'
         temp+=`<td><button class='btn btn-secondary' data-toggle='modal' data-target='#modal-${index}' >View Details</button></td>`
-        temp+='<td><button class="btn btn-primary">Make Reservation</button></td></tr>'
+        
+        
+        temp+=`</tr>`
         })
+        
 
         document.getElementById('tbody').innerHTML=temp
         
@@ -25,6 +34,85 @@ fetch('http://localhost:3000/destinations')
 
     }
 })
+}
+
+
+    const name = document.querySelector("#name")
+    const surname = document.querySelector("#surname")
+    const email = document.querySelector("#email")
+    const jmbg = document.querySelector("#jmbg")
+    const note = document.querySelector("#note")
+    const sendBtn = document.querySelector("#send")
+    const viewRes = document.querySelector("#viewRes")
+    const content  = document.querySelector(".content")
+    const tableRes = document.querySelector('.tableRes')
+    const goBackBtn= document.querySelector("#goBackBtn")
+    const destination = document.querySelector('#destination')
+    const priceRes = document.querySelector('#priceRes')
+    const nameRes = document.querySelector('#nameRes')
+   
+    
+    
+    
+        
+        
+   
+    
+
+
+    goBackBtn.addEventListener('click',()=>{
+        content.style.display='block'
+        tableRes.style.display='none'
+    })
+
+    sendBtn.addEventListener("click",()=>{
+        const data = {'name':name.value,'surname':surname.value,'destination':destination.value,'email':email.value,'jmbg':jmbg.value,'note':note.value}
+        fetch('http://localhost:3000/reservations',{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(data)
+    }).then(res=>res.json())
+      .then(data=>console.log('success',data))
+    })
+    
+    viewRes.addEventListener('click',()=>{
+        content.style.display='none'
+        tableRes.style.display='block'
+        fetch('http://localhost:3000/reservations')
+        .then(res=>res.json())
+        .then(data=>{
+        if(data.length>0){
+        $(document).ready( function () {
+            $('table1').DataTable();
+        } );
+        let temp=''   
+        
+        data.forEach((item,index)=>{
+        temp+='<tr>'
+        temp+='<td>'+item.name+'</td>'
+        temp+='<td>'+item.surname+'</td>'
+        temp+='<td>'+item.destination+'</td>'
+        temp+='<td>'+item.email+'</td>'
+        temp+='<td>'+item.note+'</td>'
+        temp+=`<td><button class='btn btn-secondary' data-toggle='modal' data-target='#modal-${index}' >Change Reservation</button></td>`
+        temp+=`<td><button class='btn btn-primary' data-toggle='modal' data-target='#modalRes'  >Delete Reservation</button></td>`
+        
+        temp+=`</tr>`
+        })
+        
+
+        document.getElementById('tbody1').innerHTML=temp
+        
+        
+
+    }
+})
+
+
+    })
+
+
+
 
 
 
